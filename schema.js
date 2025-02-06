f8cd467769d3805ab723e6a194d29641a2c91d71
VRAJPATEL621204 <vrajpatel621204@gmail.com>
2025-02-06 11:08:02 +0530

    Assignment completed

---
+++ schema.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const model = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+    },
+    price:{
+        type:Number,
+        required:true,
+    }
+
+})
+
+
+module.exports=mongoose.model("model",model)
+
+
